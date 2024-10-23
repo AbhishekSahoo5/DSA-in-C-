4b6f50509bbd3a24b3569e85289a873b13d64e98
Abhishek Sahoo <abhisheksahoo553@gmail.com>
2024-10-23 10:45:52 +0530

    Lecture 2

---
+++ L002_FirstPrograminC++/firstProgram.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    cout<<"Hello World,"<<endl<<"Abhishek This side"<<endl;
+
+    int a=123;
+    cout<<a<<endl;
+    int b;
+    cout<<b<<endl;
+    cout<<sizeof(b)<<endl;
+
+    char c='a';
+    cout<<c<<" "<<sizeof(c)<<endl;
+
+}
---
+++ L002_FirstPrograminC++/typecasting.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // TypeCasting
+
+    int a='a';
+    cout<<a<<endl;
+    char b=98;
+    cout<<b<<endl;
+
+    
+}
