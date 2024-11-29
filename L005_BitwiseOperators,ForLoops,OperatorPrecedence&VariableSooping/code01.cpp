c051a25e575d7bd73279add59f6dafad13b35924
Abhishek Sahoo <abhisheksahoo553@gmail.com>
2024-11-29 16:31:45 +0530

    Bitwise

---
+++ L005_BitwiseOperators,ForLoops,OperatorPrecedence&VariableSooping/code01.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a=4;
+    int b=6;
+
+    cout<<"a&b= "<<(a&b)<<endl;
+    cout<<"a|b= "<<(a|b)<<endl;
+    cout<<"~a= "<<~a<<endl;
+    cout<<"a^b= "<<(a^b)<<endl;
+
+    //  Left and Right shift
+    // cout<<"5<<1"<<(5<<1)<<endl;
+    cout<<"17>>1"<<(17>>1)<<endl;
+
+
+    return 0;
+}
---
+++ L006_Binary&DecimalNumberSystem/Code1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+// Decimal to Binary
+
+
+int  main(){
+    int n;
+    cout<<"Enter n";
+    cin>>n;
+
+    while()
+}
